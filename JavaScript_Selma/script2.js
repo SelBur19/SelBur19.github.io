efa631bf887315fa8302ac463d29aa1845276467
SelBur19 <94104535+SelBur19@users.noreply.github.com>
2023-02-14 11:09:11 +0100

    Add files via upload

---
+++ JavaScript_Selma/script2.js
@@ -0,0 +1,16 @@
+//Der prompt - Befehl erzeugt ein Fenster mit Eingabenfeld.
+//prompt("Wie alt bist du?")
+
+//alert(prompt("Wie alt bist du?"));
+
+//Kombination von Text un Eingabe:
+//alert(prompt("Wie alt bist du ?"));
+
+//Mit + werden einzelne feile kombiniert
+
+alert("Du bist  "+prompt("Wie alt bist du?")+" Jahren alt.");
+
+
+
+
+
